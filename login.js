c273b23cc93dbcce51b557b1e1670baa7428d148
StefanM <stefanmiloiu@live.com>
2022-08-22 23:07:52 +0300

    added comments

--- login.js
+++ login.js
@@ -1,8 +1,11 @@
-const clickBtn = document.querySelector("#clickBtn");
-clickBtn.addEventListener("click", generateContent, false);
+const clickBtn = document.querySelector("#clickBtn"); //getting easy to use the button - creating a shortcut
+clickBtn.addEventListener("click", generateContent, false); //looking for action at the button and putting the instruction
 
 function generateContent(e) {
-  let fragment = new DocumentFragment();
+  removeAllChildNodes(mainframe); //cleaning the mainframe, if not,
+  //it creates row after rows and get long
+  let fragment = new DocumentFragment(); //using fragment, a kind of intermediary
+  //bucket
   let numberOfItems = 16;
 
   for (let i = 0; i < numberOfItems; i++) {
@@ -13,9 +16,17 @@ function generateContent(e) {
       let divElementC = document.createElement("div");
       divElementC.classList.add("itemC");
       divElementR.appendChild(divElementC);
-    }
+    } //creating 16 columns in each row
     fragment.appendChild(divElementR);
-  }
+  } //creating rows and put the in the "bucket" fragment
 
-  mainframe.appendChild(fragment);
+  mainframe.appendChild(fragment); //moving the content from the fragment
+  //to the actual target -> mainframe
 }
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+} //removing all the children of the parent
+//to add js mouseover
